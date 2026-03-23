872d5682997a09ac2ffeff89ff90f0275a759922
Ousmane199 <106654657+Ousmane199@users.noreply.github.com>
2026-03-23 15:27:55 +0100

    Modification test

--- src/test/java/fr/utc/miage/shares/UserTest.java
+++ src/test/java/fr/utc/miage/shares/UserTest.java
@@ -1,6 +1,53 @@
 package fr.utc.miage.shares;
 
-public class UserTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    @Test
+    void testGetFirstName() {
+        User user = new User("Doe", "John");
+        assertEquals("John", user.getFirstName());   }
+
+    @Test
+    void testGetName() {
+        User user = new User("Doe", "John");
+        assertEquals("Doe", user.getName());
+
+    }
+
+    @Test
+    void testGetPortefeuille() {
+        User user = new User("Doe", "John");
+        assertEquals("Doe John", user.getPortefeuille().getNom());
+        assertEquals("Standard", user.getPortefeuille().getType());
+        assertEquals(Map.of(), user.getPortefeuille().getMapActions());
+
+    }
+
+    @Test
+    void testSetFirstName() {
+        User user = new User("Doe", "John");
+        user.setFirstName("Jane");
+        assertEquals("Jane", user.getFirstName());
+    }
+
+    @Test
+    void testSetName() {
+        User user = new User("Doe", "John");
+        user.setName("Smith");
+        assertEquals("Smith", user.getName());
+
+    }
+
+    @Test
+    void testSetPortefeuille() {
+        User user = new User("Doe", "John");
+        Portefeuille portefeuille = new Portefeuille("Doe John", "Standard", Map.of());
+        user.setPortefeuille(portefeuille);
+        assertEquals(portefeuille, user.getPortefeuille());
+    }
 }
