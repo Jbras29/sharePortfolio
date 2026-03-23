16a2f9ce27e0f9ac9a356e121885de8b5dc38904
Ousmane199 <106654657+Ousmane199@users.noreply.github.com>
2026-03-23 15:11:38 +0100

    Creation de la classe user

---
+++ src/test/java/fr/utc/miage/shares/PortefeuilleTest.java
@@ -0,0 +1,40 @@
+package fr.utc.miage.shares;
+
+import org.junit.jupiter.api.Test;
+
+public class PortefeuilleTest {
+    @Test
+    void testAfficher() {
+
+    }
+
+    @Test
+    void testContientAction() {
+
+    }
+
+    @Test
+    void testGetMapActions() {
+
+    }
+
+    @Test
+    void testGetNom() {
+
+    }
+
+    @Test
+    void testGetType() {
+
+    }
+
+    @Test
+    void testSetMapActions() {
+
+    }
+
+    @Test
+    void testSetType() {
+
+    }
+}
