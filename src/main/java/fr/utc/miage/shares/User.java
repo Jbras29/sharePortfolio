82cda83c17daee6ab7c92d268018210ce0c0142e
Ousmane199 <cisseousmane3208@gmail.com>
2026-03-30 12:02:53 +0200

    Modification

--- src/main/java/fr/utc/miage/shares/User.java
+++ src/main/java/fr/utc/miage/shares/User.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 David Navarre &lt;David.Navarre at irit.fr&gt;.
+ * Copyright 2025 David Navarre <David.Navarre at irit.fr>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,35 +58,25 @@ public class User {
 
     public Map<Action, Integer> quantitePossede() {
         Map<Action, Integer> result = new HashMap<>();
-
         if (portefeuille != null) {
             result.putAll(portefeuille.getMapActions());
         }
-
         return result;
     }
-  
+
     public ActionDTO getInfoAction(Action action) {
         LocalDate today = LocalDate.now();
         Jour jourActuel = new Jour(today.getYear(), today.getMonthValue(), today.getDayOfMonth());
         return new ActionDTO(action.getLibelle(), action.getTypeAction(), action.valeur(jourActuel));
-    } 
-<<<<<<< HEAD
-=======
+    }
 
     public float valeurPortefeuille(Jour jour) {
-    float valeurTotale = 0;              // 1. On commence à 0€
-    
-    for (Map.Entry<Action, Integer> entry : this.portefeuille.getMapActions().entrySet()) 
-    { 
-        Action action = entry.getKey();   
-        int quantite = entry.getValue();  
-        
-        valeurTotale += action.valeur(jour) * quantite;
-        
+        float valeurTotale = 0;
+        for (Map.Entry<Action, Integer> entry : this.portefeuille.getMapActions().entrySet()) {
+            Action action = entry.getKey();
+            int quantite = entry.getValue();
+            valeurTotale += action.valeur(jour) * quantite;
+        }
+        return valeurTotale;
     }
-    
-    return valeurTotale; 
-}
->>>>>>> 3e0a860cfd5cce967a93b08ae0b1668ed64ba61f
 }
